278f39a26d731ab40bcb6379a634365a8f336a04
Rahul Subramanian <emailrahuls@gmail.com>
2021-11-08 22:36:55 -0800

    Combined the signals into a bus. Need to create a non-virtual bus to be able to exit the model reference subsystem. Need to test the queue populator to check it's working for all conditions.

--- test_vector.m
+++ test_vector.m
@@ -2,12 +2,13 @@
 % TEST DATA
 %=========================================================================%
 
-test_input.test_container = uint8([0:5]);
+test_input.test_container = 0*uint8([0:5]);
 test_input.front_index = uint8(0);
-test_input.back_index_start = uint8(3);
+test_input.back_index_start = uint8(0);
 test_input.new_value = uint8(10);
-test_input.threshold = uint8(10);
-test_input.queue_size_starting = uint8(3);
+test_input.threshold = uint8(0);
+% test_input.threshold = uint8(0);
+test_input.queue_size_starting = uint8(0);
 
 %=========================================================================%
 % END
